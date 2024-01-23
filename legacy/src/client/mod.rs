9b3ce9e680ddc849f643ea8fca3e83fb0a7ad966
Jun Kurihara <junkurihara@users.noreply.github.com>
2024-01-23 21:58:40 +0900

    remove legacy

--- legacy/src/client/mod.rs
+++
@@ -1,9 +0,0 @@
-mod cache;
-mod credential;
-mod doh_client;
-mod http_bootstrap;
-mod odoh;
-
-pub use cache::Cache;
-pub use credential::Credential;
-pub use doh_client::{DoHClient, DoHMethod, DoHType};
