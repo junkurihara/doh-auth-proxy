77ffcf1cdc844144d88ef94f768ed1f9f8bba0c6
Jun Kurihara <junkurihara@users.noreply.github.com>
2024-05-20 19:49:27 +0900

    chore: update tracing code

--- proxy-bin/src/log.rs
+++ proxy-bin/src/log.rs
@@ -1,25 +1,24 @@
 pub use tracing::{error, info, warn};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 pub fn init_logger() {
-  use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+  let global_level_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
-  let format_layer = fmt::layer()
-    .with_line_number(false)
+  // This limits the logger to emits only this crate with any level, for included crates it will emit only INFO or above level.
+  let stdio_layer = fmt::layer()
+    .with_line_number(true)
     .with_thread_ids(false)
-    .with_target(false)
     .with_thread_names(true)
     .with_target(true)
     .with_level(true)
-    .compact();
+    .compact()
+    .with_filter(tracing_subscriber::filter::filter_fn(move |metadata| {
+      metadata
+        .target()
+        .starts_with(env!("CARGO_PKG_NAME").replace('-', "_").as_str())
+        || metadata.level() <= &tracing::Level::INFO
+    }));
 
-  // This limits the logger to emits only proxy crate
-  let pkg_name = env!("CARGO_PKG_NAME").replace('-', "_");
-  let level_string = std::env::var(EnvFilter::DEFAULT_ENV).unwrap_or_else(|_| "info".to_string());
-  let filter_layer = EnvFilter::new(format!("{}={}", pkg_name, level_string));
-  // let filter_layer = EnvFilter::from_default_env();
-
-  tracing_subscriber::registry()
-    .with(format_layer)
-    .with(filter_layer)
-    .init();
+  let reg = tracing_subscriber::registry().with(global_level_filter).with(stdio_layer);
+  reg.init();
 }
